f40461dde9764cc41237198e1ecc680b07128952
ShubhamTiwari55 <stofficial059@gmail.com>
2023-09-26 20:04:10 +0530

    updated axios instance

--- Lms_fronted/src/App.jsx
+++ Lms_fronted/src/App.jsx
@@ -5,12 +5,14 @@ import { Route,Routes } from 'react-router-dom';
 import AboutUs from './Pages/AboutUs';
 import HomePage from './Pages/HomePage';
 import NotFound from './Pages/NotFound';
+import SignUp from './Pages/SignUp';
 function App() {
   return (
     <>
       <Routes>
       <Route path="/" element={<HomePage/>}></Route>
       <Route path="/about" element={<AboutUs/>}></Route> 
+      <Route path="/signup" element={<SignUp/>}></Route>
       <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </>
