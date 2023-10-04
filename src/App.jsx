59600ac3c44608064e954dc85b25ac4fa1009d84
ShubhamTiwari55 <stofficial059@gmail.com>
2023-10-05 00:48:56 +0530

    added till signup and login

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+import { Route,Routes } from 'react-router-dom';
+
+import AboutUs from './Pages/AboutUs';
+import HomePage from './Pages/HomePage';
+import Login from './Pages/Login'
+import NotFound from './Pages/NotFound';
+import SignUp from './Pages/SignUp';
+function App() {
+  return (
+    <>
+      <Routes>
+      <Route path="/" element={<HomePage/>}></Route>
+      <Route path="/about" element={<AboutUs/>}></Route> 
+      <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
