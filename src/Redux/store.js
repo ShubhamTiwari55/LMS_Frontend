63323dfc68a2ed07371f48d1cd26c767ea8be9cb
ShubhamTiwari55 <stofficial059@gmail.com>
2023-10-05 19:12:09 +0530

    logout implemented

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authSliceReducer from "./slices/AuthSlice";
+import courseSliceReducer from './slices/courseSlice'
 
 const store = configureStore({
     reducer: {
-        auth: authSliceReducer
+        auth: authSliceReducer,
+        course: courseSliceReducer
     },
     devTools: true
 });
--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
 
 import Footer from '../Components/Footer';
+import { logout } from '../Redux/slices/AuthSlice';
 function HomeLayout({children}){
 
     const dispatch = useDispatch();
@@ -26,10 +27,10 @@ function HomeLayout({children}){
         element[0].checked = false;
     }
 
-    function handleLogout(e){
+    async function handleLogout(e){
         e.preventDefault();
-        // const res = await dispatch(logout());
-        // if(res?.payload?.success)
+        const res = await dispatch(logout());
+        if(res?.payload?.success)
         navigate('/');
     }
     return(
