22190632ba380cbdf8a6308f3785990b37a4ca68
ShubhamTiwari55 <stofficial059@gmail.com>
2023-10-06 16:27:28 +0530

    contact us page updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route,Routes } from 'react-router-dom';
 
 import CourseList from './Course/CourseList';
 import AboutUs from './Pages/AboutUs';
+import ContactUs from './Pages/Contact';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login'
 import NotFound from './Pages/NotFound';
@@ -17,6 +18,7 @@ function App() {
       <Route path="/signup" element={<SignUp/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/courses" element={<CourseList/>}></Route>
+      <Route path="/contact" element={<ContactUs/>}></Route>
       <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </>
