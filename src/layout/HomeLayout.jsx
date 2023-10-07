963d3b7f58d1d897da9e8dfa8a13ad4cc1ce4146
ShubhamTiwari55 <stofficial059@gmail.com>
2023-10-07 12:40:21 +0530

    course creation by admin implemented successfully

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -66,6 +66,13 @@ function HomeLayout({children}){
                                 </Link>
                             </li>
                         )}
+                        {isLoggedIn && role == 'ADMIN' && (
+                            <li>
+                                <Link to="/course/create">
+                                    Create new Course
+                                </Link>
+                            </li>
+                        )}
                         <li>
                         <Link to="/courses"> All Courses</Link>
                         </li>
