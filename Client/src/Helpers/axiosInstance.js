0af722e30a318d92fd60193291c233e7403acd34
ShubhamTiwari55 <stofficial059@gmail.com>
2024-02-29 14:15:03 +0530

    added vercel.json file

--- Client/src/Helpers/axiosInstance.js
+++ Client/src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5014/api/v1";
+const BASE_URL = "https://lms-full-stack-783y.vercel.app/";
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
